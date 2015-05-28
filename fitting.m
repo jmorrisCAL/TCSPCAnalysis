409a5b88a51a7522af8c84370806e608d69e2f89
Jonathan Morris <jmorris@berkeley.edu>
2015-05-28 15:04:04 -0700

    Confidence interval added to plot.

--- fitting.m
+++ fitting.m
@@ -14,11 +14,6 @@ params = [A_l tau_l A_s tau_s];
 signal = signalModel(params, t, irf);
 signal = poissrnd(signal);
 
-figure;
-plot(t, irf);
-hold on;
-scatter(t, signal);
-
 %% fit long lifetime
 %[p_l, s_l] = polyfit(t(n-long:n),log(signal(n-long:n)),1);
 %param_l0(1) = exp(p_l(2));
@@ -40,14 +35,33 @@ func = @(params, times)signalModel(params, times, irf);
                                    lowerBound, ...
                                    upperBound, ...
                                    gridpoints)
-
+% Let's do it once more with the optimal values as the guess in order to get
+% the jacobian to estimate the confidence interval... because this is easier
+% than storing all of the jacobians since they're stored in a funky data
+% structure
+[opt, resNorm, res, eFlag, out, lambda, jac] = lsqcurvefit(func, ...
+                                                           opt, ...
+                                                           t, ...
+                                                           signal, ...
+                                                           lowerBound, ...
+                                                           upperBound);
+confidenceInterval = nlparci(opt,res,'jacobian',jac);
 % plot fit vs signal
 figure;
 scatter(t, signal);
 hold on;
-plot(t, signalModel(opt, t, irf));
+plot(t, signalModel(opt, t, irf),'r');
+plot(t, signalModel(confidenceInterval(:,1), t, irf),'r--');
+plot(t, signalModel(confidenceInterval(:,2), t, irf),'r--');
+title('Fit vs Signal with 95% Confidence Interval');
+xlabel('Time (ps)');
+ylabel('Counts');
+legend('Signal', 'Fit', '95% CI');
 
 
 %plot residuals
 figure;
 scatter(t,res);
+title('Residuals of Fit');
+xlabel('Timepoint');
+ylabel('Residual');
